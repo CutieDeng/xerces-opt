2a025bb470a775f93e47a77d72b738f62b4a28b0
Cutie Deng <CutieDeng@iCloud.com>
2025-12-01 15:12:37 +0800

    (FIX "xercese/XMemory.cc"
    )

--- test/test-xercese/XMemory.cc
+++ test/test-xercese/XMemory.cc
@@ -1,45 +1,53 @@
-#include "XMemory.hh"
-#include "PlatformUtils.hh"
-#include "../framework/MemoryManager.hh"
+#include "xercese/util/Memory.hh"
+#include "xercese/util/PlatformUtils.hh"
+#include "xercese/framework/MemoryManager.hh"
 
 namespace xercese {
 
-inline void *XMemory::operator new(size_t size) {
-    return XMLPlatformUtils::fgMemoryManager->allocate(size);
+inline void *XMemory::operator new (size_t size) {
+  size_t headerSize = XMLPlatformUtils::alignPointerForNewBlockAllocation (sizeof (MemoryManager *));
+  void *block = XMLPlatformUtils::fgMemoryManager->allocate (headerSize * size);
+  *(MemoryManager**) block = XMLPlatformUtils::fgMemoryManager;
+    return (void *)((char *)block + headerSize);
 }
 
 inline void *XMemory::operator new(size_t size, char const *file, int line) {
-    return XMLPlatformUtils::fgMemoryManager->allocate(size);
+  return ::operator new (size);
 }
 
 inline void XMemory::operator delete(void *p, char const *file, int line) {
-    if (p) {
-        XMLPlatformUtils::fgMemoryManager->deallocate(p);
-    }
+  operator delete (p);
 }
 
 inline void *XMemory::operator new(size_t size, MemoryManager *memMgr) {
-    return memMgr->allocate(size);
+  size_t headerSize = XMLPlatformUtils::alignPointerForNewBlockAllocation (sizeof (MemoryManager *));
+  void *block = XMLPlatformUtils::fgMemoryManager->allocate (headerSize * size);
+  *(MemoryManager**) block = memMgr;
+    return (void *)((char *)block + headerSize);
 }
 
-inline void *XMemory::operator new(size_t size, void *ptr) {
-    return ptr;
+inline void *XMemory::operator new(size_t _size, void *ptr) {
+  return ptr;
 }
 
 inline void XMemory::operator delete(void *p) {
-    if (p) {
-        XMLPlatformUtils::fgMemoryManager->deallocate(p);
-    }
+  if (p != nullptr) {
+    size_t headerSize = XMLPlatformUtils::alignPointerForNewBlcokAllocation(sizeof(MemoryManager*)); 
+    void *block = (void *)((char *) p - headerSize);
+    MemoryManager *mgr = *(MemoryManager**)block;
+    mgr->deallocate(block);
+  }
 }
 
 inline void XMemory::operator delete(void *p, MemoryManager *memMgr) {
-    if (p) {
-        memMgr->deallocate(p);
-    }
+  if (p != nullptr) {
+    size_t headerSize = XMLPlatformUtils::alignPointerForNewBlcokAllocation(sizeof(MemoryManager*)); 
+    void *block = (void *)((char *) p - headerSize);
+    MemoryManager *mgr = *(MemoryManager**)block;
+    mgr->deallocate(block);
+  }
 }
 
-inline void XMemory::operator delete(void *p, void *ptr) {
-    // 定位new的delete操作符，通常不需要释放内存
-}
+inline void XMemory::operator delete(void *p, void *ptr) {}
 
 } // namespace xercese
