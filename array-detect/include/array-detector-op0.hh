e056973d7233a512825df594dcbf4d2f21e6596d
Cutie Deng <CutieDeng@iCloud.com>
2025-12-04 00:08:32 +0800

    (CHORE ""
    "mv analyze_field_assignments_in_function"
    )

--- array-detect/include/array-detector-op0.hh
+++ array-detect/include/array-detector-op0.hh
@@ -6,7 +6,10 @@
 
 namespace array_detector {
 
+using namespace cutie_ns;
+
 class ArrayDetector;
+CutieErrorCode analyze_field_assignments_in_functions(ArrayDetector &self, CUTIE_FUNC_ARGS);
 
 } // namespace array_detector
 
@@ -15,7 +18,6 @@ namespace cutie_ns {
 using namespace array_detector;
 
 CutieErrorCode collect_all_types_and_fields(CUTIE_FUNC_ARGS, ArrayDetector* detector);
-CutieErrorCode analyze_field_assignments_in_functions(CUTIE_FUNC_ARGS, ArrayDetector* detector);
 
 CutieErrorCode array_detect_execute(CUTIE_FUNC_ARGS);
 CutieErrorCode array_detect_analysis(CUTIE_FUNC_ARGS);
