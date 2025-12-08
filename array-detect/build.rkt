03fe83fc31d26bef5efec5d9f25f2044fba84f4f
Cutie Deng <CutieDeng@iCloud.com>
2025-12-08 10:58:59 +0800

    (FIX "build.rkt: cutie_state.txt"
    )

--- array-detect/build.rkt
+++ array-detect/build.rkt
@@ -256,15 +256,16 @@
       (match-define `(,i ,o ,p ,i2 ,h) (apply process* (append `(,cxx . ,args^) `("-M" ,filename))))
       (h 'wait)
       (define is (sequence->list (in-lines i)))
-      (match (h 'status)
-        ['done-ok
-          (for/list ([j (in-naturals)] [f (in-list is)])
-            (define f^ (cond [(equal? (+ j 1) (length is)) f] [else (string-trim f " \\" #:left? #f)]))
-            (define f^^ (cond [(equal? j 0) (define loc (string-find f^ ": ")) (substring f^ (+ loc 2))] [else (string-trim f^ #:right? #f #:repeat? #t)]))
-            f^^
-          )]
-        ['done-error
-          (raise-user-error 'calc-dependency "failed to calc '~a' by ~a'" filename cxx)])
+      (append*
+        (match (h 'status)
+          ['done-ok
+            (for/list ([j (in-naturals)] [f (in-list is)])
+              (define f^ (cond [(equal? (+ j 1) (length is)) f] [else (string-trim f " \\" #:left? #f)]))
+              (define f^^ (cond [(equal? j 0) (define loc (string-find f^ ": ")) (substring f^ (+ loc 2))] [else (string-trim f^ #:right? #f #:repeat? #t)]))
+              (string-split f^^)
+            )]
+          ['done-error
+            (raise-user-error 'calc-dependency "failed to calc '~a' by ~a'" filename cxx)]))
     )
   )
 )
