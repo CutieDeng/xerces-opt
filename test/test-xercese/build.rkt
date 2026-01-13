16722ed996bbeae81799f0056c26b96fdb94c58d
Cutie Deng <CutieDeng@iCloud.com>
2026-01-13 19:05:45 +0800

    (FIX "test: xercese build"
    )

--- test/test-xercese/build.rkt
+++ test/test-xercese/build.rkt
@@ -3,26 +3,26 @@
 (require "../../array-detect/test-script/test-lib.rkt")
 (require "../../array-detect/test-script/test-lib2.rkt")
 
+(define test-out "out")
+
 (define ut-compile-task-files
-  '(("MemoryManagerImpl.cc" "test-out/MemoryManagerImpl.o")
-    ("PlatFormUtils.cc" "test-out/PlatFormUtils.o")
-    ("main_test.cpp" "test-out/main_test.o")
-    ("XMemory.cc" "test-out/XMemory.o")))
+  `(("MemoryManagerImpl.cc" ,(build-path test-out "MemoryManagerImpl.o"))
+    ("PlatFormUtils.cc" ,(build-path test-out "PlatFormUtils.o"))
+    ("main_test.cpp" ,(build-path test-out "main_test.o"))
+    ("XMemory.cc" ,(build-path test-out "XMemory.o")))
 
 (define total-compile-task-files
-  '(("test-out/MemoryManagerImpl.o"
-     "test-out/PlatFormUtils.o"
-     "test-out/main_test.o"
-     "test-out/XMemory.o")
-    "test-out/test-xercese"))
+  `(
+    ,(map cadr (force ut-compile-task-files))
+    ,(build-path test-out "test-xercese"))
 
 (define base-args '("-std=c++17" "-Wall" "-Wextra" "-g"))
 
 (define (pre root)
-  (system* (find-executable-path "mkdir") "-p" (build-path root "test-out")))
+  (system* (find-executable-path "mkdir") "-p" (build-path root test-out)))
 
 (define (run _cxx _flags root)
-  (system (path->string (build-path root "test-out" "test-xercese"))))
+  (system (path->string (build-path root test-out "test-xercese"))))
 
 (provide config)
 
